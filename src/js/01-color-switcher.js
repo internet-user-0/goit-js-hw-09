1ff35e710cfee0f17d65e94c18d1c74a6fde07dc
Anton Zvertanovsky <anton.zv2002@gmail.com>
2022-08-09 18:16:18 +0300

    добавил папку src, не работает npm start

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,19 @@
+
+const btnEl = {
+   start: document.querySelector('#data-start'),
+}
+
+
+console.log(btnEl.start);
+
+
+
+
+
+
+
+
+
+function getRandomHexColor() {
+   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+ }
